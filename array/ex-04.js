9573fc1f124934096d38be12fb7c0cc993dfa360
Francisco Viana <franciscoviana862@gmail.com>
2021-11-20 23:15:44 -0300

    feat: add ex sort

---
+++ array/ex-04.js
@@ -0,0 +1,10 @@
+/*
+*
+* Exemplo de com usar os metodos nativos do Array
+* includes | sort
+*
+*/
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log('includes number 9: ',numbers.includes(9));
+
