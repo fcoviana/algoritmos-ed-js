2517c99ea5034fc79be7939cd282229d81178b0f
Francisco Viana <franciscoviana862@gmail.com>
2021-11-21 23:20:56 -0300

    feat(ex-04.js): add ex sort

--- array/ex-04.js
+++ array/ex-04.js
@@ -8,3 +8,10 @@ let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 console.log('includes number 9: ',numbers.includes(9));
 
+const friends = [
+  { name: 'Luiz Savio', age: 20 },
+  { name: 'Paulo Italo', age: 32 },
+  { name: 'Deivid Linz', age: 18 },
+];
+
+console.log('sort friends: ', friends.sort((a, b) => a.age - b.age));
