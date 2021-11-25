7eb16bff0f2521fe356b5e93581d95e466264069
Francisco Viana <franciscoviana862@gmail.com>
2021-11-25 00:09:02 -0300

    feat(ex-05.js): add ex pesquisa

--- array/ex-05.js
+++ array/ex-05.js
@@ -1,14 +1,22 @@
 /*
 *
 * Exemplo de com usar os metodos nativos do Array
-* includes | indexOf | lastIndexOf
+*  indexOf | lastIndexOf | find | findIndex
 *
 */
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 numbers.push(12);
+numbers.push(13);
 
-console.log('index number 12: ',numbers.indexOf(12));
-console.log('index number 100: ',numbers.indexOf(100));
+console.log('index number 12: ', numbers.indexOf(12));
+console.log('index number 100: ', numbers.indexOf(100));
 
-console.log('index number 12: ',numbers.lastIndexOf(12));
-console.log('index number 100: ',numbers.lastIndexOf(100));
+console.log('index number 12: ', numbers.lastIndexOf(12));
+console.log('index number 100: ', numbers.lastIndexOf(100));
+
+function multipleOf2(element, index, array) {
+  return (element % 2 == 0);
+}
+
+console.log('find number: ', numbers.find(multipleOf2));
+console.log('findIndex number: ', numbers.findIndex(multipleOf2));
