20bc2ba3da8e647036a214f27f6c65aebb13ee5c
Francisco Viana <franciscoviana862@gmail.com>
2021-11-24 23:57:42 -0300

    feat(ex-05.js): add ex pesquisa

---
+++ array/ex-05.js
@@ -0,0 +1,11 @@
+/*
+*
+* Exemplo de com usar os metodos nativos do Array
+* includes | indexOf | lastIndexOf
+*
+*/
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+numbers.push(12);
+
+console.log('index number 12: ',numbers.indexOf(12));
+console.log('index number 100: ',numbers.indexOf(100));
